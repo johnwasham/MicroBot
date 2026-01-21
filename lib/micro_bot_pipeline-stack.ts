8f0c99166585d2656951bf1b72b5fca9617b61c5
John Washam <code@readyprompt.com>
2026-01-20 18:29:59 -0800

    pipeline rename

--- lib/micro_bot_pipeline-stack.ts
+++ lib/micro_bot_pipeline-stack.ts
@@ -7,7 +7,7 @@ export class MicroBotPipelineStack extends cdk.Stack {
     super(scope, id, props);
 
     const pipeline = new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'MicroBotPipelineStack',
+      pipelineName: 'MicroBotService',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('johnwasham/MicroBot', 'main'),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
